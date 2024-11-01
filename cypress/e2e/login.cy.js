540ebf355cbfdfac2cd47d559f7bb418103d9eb1
hediye-ersan <hediyesnl9@gmail.com>
2024-11-01 17:29:34 +0300

    formun başarı ile doldurulma testi.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
+describe('Login Page', () => {
   it('passes', () => {
     cy.visit('http://localhost:5173/')
   })
-})
+});
+it(' Başarılı form doldurulduğunda submit edebiliyorum:', () => {
+  cy.get('input[name="email"]').type('valid@example.com'); // Geçerli bir email yaz
+  cy.get('input[name="password"]').type('validPassword'); // Geçerli bir şifre yaz
+  cy.get('input[name="terms"]').check(); // Kullanım şartlarını kabul et
+  cy.get('button[type="submit"]').click(); // Gönder butonuna tıkla
+
+  // Başarılı sayfaya yönlendirildiğini kontrol et
+  cy.url().should('include', '/success'); // Başarılı sayfanın URL'sini kontrol et
+});
