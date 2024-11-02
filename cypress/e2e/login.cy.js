0f429333379d61ce05779df32cdb0d11a0551ff0
hediye-ersan <hediyesnl9@gmail.com>
2024-11-02 17:53:56 +0300

    cy dosyası güncellendi.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,44 @@
 
-describe("A senaryosu testi", ()=>{
-  it("Başarılı form doldurulduğunda submit edebiliyorum:", () => {
-    cy.visit("http://localhost:5173/")
-    cy.get('[data-cy="email"]').type("erdem.guntay@wit.com.tr")
-    cy.get('[data-cy="password"]').type("9fxIH0GXesEwH_I ")
-    cy.get('[data-cy="terms"]').check()
-    cy.get('[data-cy="submit"]').click()
+
+
+  describe('Login Form Tests', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(true);
+    })
+  });
+
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/'); // Login sayfasına git
+  });
+
+  describe("Başarılı form doldurulduğunda submit edebiliyorum:" , ()=> {
+    it("succes sayafasını açabiliyorum" , ()=> {
+      cy.get('[data-cy="email-input"]').type("erdem.guntay@wit.com.tr")
+      cy.get('[data-cy="password-input"]').type("9fxIH0GXesEwH_I")
+      cy.get('[data-cy="terms-input"]').click()
+      cy.get('[data-cy="submit"]').click
+    });
+    it("email yanlış girdim:", ()=> {
+      cy.get('[data-cy="email-input"]').type("erdem.guntay@wit.com.tr")
+      cy.get('[data-cy="error-mesage"]').should("have.length", 1)
+      cy.get('[data-cy="error-mesage"]').contains("please enter")
+
+    })
   })
-})
+
+  describe ("Basarılı testler:", ()=>{
+    it("succes" , ()=> {
+      cy.get('[data-cy="email-input"]').type("erdem.guntay@wit.com.tr")
+      cy.get('[data-cy="password-input"]').type("9fxIH0GXesEwH_I")
+      cy.get('[data-cy="terms-input"]').check()
+      cy.get('[data-cy="submit"]').click
+      cy.url().should("contain","/main")
+    });
+  })
+  
+
+ 
+
 /* Proje oluşturulacak
 
 2. Cypress ile de 2 farklı senaryoda test yazılacak:
